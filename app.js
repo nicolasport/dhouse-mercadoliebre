791c0b3082ee6c4b3b84964f4c00bfaea9bb8729
nicolasporte <nicolas.lp.30@gmail.com>
2021-10-07 01:36:35 -0300

    B.2
    C3

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+var path = require('path');
+const app = express()
+const port = 3030
+
+
+var filePage = {
+    index: '/view/home.html',
+
+}
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, filePage['index']));
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
