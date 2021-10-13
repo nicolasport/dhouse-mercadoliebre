12b8d0567c83dc54f3d0f7ef604beeca7d8fef2f
nicolasporte <nicolas.lp.30@gmail.com>
2021-10-13 02:00:20 -0300

    LOGIN and REGISTER HTML

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 var path = require('path');
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,15 +7,35 @@ const port = process.env.PORT || 3000
 
 var filePage = {
     index: '/view/home.html',
+    register: '/view/register.html',
+    login: '/view/login.html',
 
 }
 
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, filePage['index']));
 })
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, filePage.register));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, filePage.login));
+})
+app.post('/login',(req, res) => {
+    var user_name = req.body.username;
+    var password = req.body.password;
+    console.log(`Nombre de usuario: ${user_name} y contraseña: ${password}`);
+    res.redirect("/")
+});
+app.post('/register',(req, res) => {
+    console.log(req.body);
+    res.redirect("/")
+});
 
 
 app.listen(port, () => {
